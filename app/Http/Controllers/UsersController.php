d75f1a391fc77c430155f368f003c89140aebc5f
Anas Talal <anasctalal@gmail.com>
2022-08-25 17:19:41 +0300

    update2

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,10 @@ use App\User;
 use Auth;
 class UsersController extends Controller
 {
+
+    public function index(){
+        return "hello world";
+    }
  public function login(){
   if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
    $user = Auth::user();
