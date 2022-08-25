524edd1e88c0caeb1f892beea9de6cabce54a503
Anas Talal <anasctalal@gmail.com>
2022-08-25 17:54:28 +0300

    updatee

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         // if (! $this->app->routesAreCached()) {
         //     Passport::routes();
         // }
-        Passport::loadKeysFrom('/secrets/oauth');
+       // Passport::loadKeysFrom(__DIR__.'/secrets/oauth');
         // Passport::loadKeysFrom(__DIR__.'/secrets/oauth');
+        Passport::hashClientSecrets();
     }
 }
